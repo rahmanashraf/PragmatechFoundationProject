af56c9ca16348052e687a3f038e1ebb53beed4c7
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-03 02:01:20 +0400

    python teze tapsirig ozunmcun eledim

---
+++ ProjectBackEnd/input.py
@@ -0,0 +1,14 @@
+# seninadinnedir=input("x: ")
+# seninyasinkacoglum=input("y: ")
+# print(seninadinnedir+seninyasinkacoglum)
+
+"""bir masin neche km yol gedibse onu mil olaraq print ele
+mil=km/1.6"""
+
+km=int(input("de: "))
+
+ekvivalent=1.6
+
+miles=km/ekvivalent
+
+print(int(miles))
