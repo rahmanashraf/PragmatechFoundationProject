6877b922e6d167cf830c0f9f66c72a9849461a02
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-03 22:59:54 +0400

    python task next

--- ProjectBackEnd/input.py
+++ ProjectBackEnd/input.py
@@ -5,10 +5,13 @@
 """bir masin neche km yol gedibse onu mil olaraq print ele
 mil=km/1.6"""
 
-km=int(input("de: "))
+km=int(input("Nechke km getmisen atam?: "))
+suret=int(input("Neche suretnen gedmisen?: "))
 
 ekvivalent=1.6
-
+zaman=km/suret
 miles=km/ekvivalent
 
-print(int(miles))
+print(f'Siz {km} kilometr yol getmekle {miles} mil yol qet etmis oldunuz')
+print('Siz'+' '+str(km)+' '+'kilometrlik yolu'+' '+str(suret)+' '+'km/saat suret ile surseniz gedeceyiniz menzile'+' '+str(zaman)+'saat muddetinde catmis olarsiniz')
+
