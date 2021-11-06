c02d13f377cdceffeac940b42aea8d7abc886da1
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-06 20:25:50 +0400

    algoritm5_is_ready

---
+++ Algorithms/CHAllange-task/algoritm5/algoritm5.js
@@ -0,0 +1,8 @@
+let ProgLan = [
+    "Java", "Javascript", "PHP", "Angular", "React", "NodeJs"
+]
+let WorldLan = [
+    "English", "Azeri", "French", "Turkish", "Russian"
+]
+var langs = ProgLan.concat(WorldLan)
+console.log(langs)
