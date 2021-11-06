bd00b0c57fd06ca37f8b769bdaf7b7bab7e91fb9
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-06 19:49:33 +0400

    algoritm2 ready

---
+++ Algorithms/CHAllange-task/algoritm2.ready/algoritm2.js
@@ -0,0 +1,8 @@
+let x = prompt("eded daxil edin")
+Number("x")
+let tam = parseInt(x)
+let y = x - tam
+let c = y.toPrecision(2)
+
+let netice = (100 * c) + tam
+document.write(netice)
--- Algorithms/CHAllange-task/task1/task1.js
+++ Algorithms/CHAllange-task/task1/task1.js
@@ -1,4 +1,8 @@
+change.addEventListener(onclick, 'changecolor()')
+
 function changecolor(arg) {
+    x += 30
     document.body.style.backgroundColor = "red";
     document.getElementById('rgbler').innerHTML = "qirmizi"
+
 }
