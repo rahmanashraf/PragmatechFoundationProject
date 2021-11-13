c44a3acb99e4d7c5ff7ede3159615023fd9e6455
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-14 02:16:47 +0400

    bir problem qalib ordada nese catmir

--- ProjectBackEnd/adlar.py
+++ ProjectBackEnd/adlar.py
@@ -9,9 +9,12 @@ adlar=[
     'Ravi'
 ]
 
-# Butun adlari ekrana cap eden metod yazin adlariGoster()
-# Listin cut yerde duran elementlerini gosteren metod yazin cutYerdekiler()
-# Listi elifba sirasina gore siralayan metod yazin listiSirala() 
+# Butun adlari ekrana cap eden metod yazin adlariGoster()-+++++++++
+# Listin cut yerde duran elementlerini gosteren metod yazin cutYerdekiler()-+++++++++++
+# Listi elifba sirasina gore siralayan metod yazin listiSirala() -+++++++++++
+#Daxilində e herfi olan adları ekrana cap edən metod yazin
+#Son hərfi i olan adlari ekrana cap eden metod yazin
+#Hərf sayi eyni olan nece ad oldugunu ekrana cap edin
 
 def adlariGoster(adlar):
     for ad in adlar:
@@ -23,18 +26,44 @@ def cutyerdekiler():
     for i in adlar:
         x=adlar.index(i)
         if x%2==0:
-            print(x)
+            return(x)
+            
         
 
 cutyerdekiler() 
 
 def listisirala():
     adlar.sort()
-    print(adlar)
+    adlariGoster(adlar)
 
 
 listisirala()
 
+def eherfintapan():
+    for abdul in adlar:
+        if abdul.find("e")!=-1:
+            print(f'{abdul} e herfi var')
+            
+        
 
 
+eherfintapan()
 
+def sonherfiIolsun():
+    for soni in adlar:
+        x=soni[-1:]
+        if x=="i":
+            print(soni)
+
+
+sonherfiIolsun()
+
+def herfsayieyni():
+    for herf in adlar:
+        c=len(herf)
+        
+        
+            
+
+
+herfsayieyni()
