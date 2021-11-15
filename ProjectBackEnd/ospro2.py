6d0c255684de0f22c52626bdc4769e04f5d3adf9
Ashrafov Rahman <rehman199577@gmail.com>
2021-11-15 21:52:28 +0400

    TASKS all completed (1,2,3,4,5)

--- ProjectBackEnd/ospro2.py
+++ ProjectBackEnd/ospro2.py
@@ -1,8 +1,10 @@
 import os
 print(os.getcwd())  ###movcud papkani print elir
 os.chdir("/Users/HP/Desktop/OsModule")
-dosya=open("file1.txt","w")
-dosya2=open("file2.txt","w")
+# dosya=open("file1.txt","w") ######
+# dosya2=open("file2.txt","w")  #####TASK2 READY
+# os.rename("file2.txt","yeniB.txt")  TASK4 READY
 
+# dosya.write("Men python yazmaga cehd elirem")  ####Task3 ready
 
-dosya.write("Men python yazmaga cehd elirem")
+# os.rename("/Users/HP/Downloads/8.png","/Users/HP/Desktop/OsModule/8.png")  ###TASK5 ready
