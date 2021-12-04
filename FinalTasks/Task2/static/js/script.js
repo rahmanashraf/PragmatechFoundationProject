5c277b1dd3f515ad7a037d7bb14e7a3ac2f40b18
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-04 22:27:49 +0400

    i have one problem

--- FinalTasks/Task2/static/js/script.js
+++ FinalTasks/Task2/static/js/script.js
@@ -1,21 +1,32 @@
-var x = document.getElementById("myAudio");
+var birSual = document.getElementById("sual1");
+var ikiSual = document.getElementById("sual2");
+var ucSual = document.getElementById("sual3");
+var dordSual = document.getElementById("sual4");
+var besSual = document.getElementById("sual5");
+var birCavab = document.getElementById("duzgun1");
+var ikiCavab = document.getElementById("duzgun2");
+var ucCavab = document.getElementById("duzgun3");
+var dordCavab = document.getElementById("duzgun4");
+var besCavab = document.getElementById("duzgun5");
+// var settime = setTimeout(birCavab.style.background = "green", 2000)
 
-function playAudio() {
-    x.play();
+
+function duzgun() {
+    if (ikiSual.style.display = "none") {
+        ikiSual.style.display = "block"
+        ucSual.style.display = "none"
+        dordSual.style.display = "none"
+        besSual.style.display = "none"
+        birSual.style.display = "none"
+
+    } else {
+        alert("sehv cavabdi")
+
+    }
 }
 
-function pauseAudio() {
-    x.pause();
-}
-
-// function basla() {
-//     var x = document.getElementById("ButtonBasla");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.transform = "translate(0,-350px)"
-//         x.innerHTML = "Uğurlar!";
-//         x.style.transition = "1s ease";
-//         setTimeout('window.open(\'http://127.0.0.1:5500/game\')', 5000)
-//     }
-// }
+//     x.style.transform = "translate(0,-350px)"
+//     x.innerHTML = "Uğurlar!";
+//     x.style.transition = "1s ease";
+//     setTimeout('window.open(\'http://127.0.0.1:5500/game\')', 5000)
+//
