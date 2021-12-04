9ca39013b8c2e14a890d8d9bb56359ca501c1c04
Ashrafov Rahman <rehman199577@gmail.com>
2021-12-05 02:56:21 +0400

    Foundation last final task bitib demek olarki uzerinde isleyeceyim seyler elave dekorasiya ve daha qisa yollarin tetbiqnin yoxlanilmasi

--- FinalTasks/Task2/static/js/script.js
+++ FinalTasks/Task2/static/js/script.js
@@ -8,10 +8,96 @@ var ikiCavab = document.getElementById("duzgun2");
 var ucCavab = document.getElementById("duzgun3");
 var dordCavab = document.getElementById("duzgun4");
 var besCavab = document.getElementById("duzgun5");
-// var settime = setTimeout(birCavab.style.background = "green", 2000)
 
 
-function duzgun() {
+
+function sehv1() {
+    document.getElementById("sehv1-1").style.backgroundColor = "red";
+    document.getElementById("sehv1-2").style.backgroundColor = "red";
+    document.getElementById("sehv1-3").style.backgroundColor = "red";
+}
+
+function sehv2() {
+    document.getElementById("sehv2-1").style.backgroundColor = "red";
+    document.getElementById("sehv2-2").style.backgroundColor = "red";
+    document.getElementById("sehv2-3").style.backgroundColor = "red";
+}
+
+function sehv5() {
+    document.getElementById("sehv5-1").style.backgroundColor = "red";
+    document.getElementById("sehv5-2").style.backgroundColor = "red";
+    document.getElementById("sehv5-3").style.backgroundColor = "red";
+}
+
+function sehv3() {
+    document.getElementById("sehv3-1").style.backgroundColor = "red";
+    document.getElementById("sehv3-2").style.backgroundColor = "red";
+    document.getElementById("sehv3-3").style.backgroundColor = "red";
+}
+
+function sehv4() {
+    document.getElementById("sehv4-1").style.backgroundColor = "red";
+    document.getElementById("sehv4-2").style.backgroundColor = "red";
+    document.getElementById("sehv4-3").style.backgroundColor = "red";
+}
+
+
+function duzgun1() {
+    if (birCavab.style.background = "#4d2065") {
+        birCavab.style.background = "green"
+        birCavab.innerHTML = "Cavab doğrudur-next(2click) "
+    } else {
+        alert("hey")
+    }
+}
+
+function duzgun2() {
+    if (ikiCavab.style.background = "#4d2065") {
+        ikiCavab.style.background = "green"
+        ikiCavab.innerHTML = "Cavab doğrudur-next(2click) "
+    } else {
+        alert("hey")
+    }
+}
+
+function duzgun3() {
+    if (ucCavab.style.background = "#4d2065") {
+        ucCavab.style.background = "green"
+        ucCavab.innerHTML = "Cavab doğrudur-next(2click) "
+    } else {
+        alert("hey")
+    }
+}
+
+
+function duzgun4() {
+    if (dordCavab.style.background = "#4d2065") {
+        dordCavab.style.background = "green"
+        dordCavab.innerHTML = "Cavab doğrudur-next(2click) "
+    } else {
+        alert("hey")
+    }
+}
+
+
+function duzgun5() {
+    if (besCavab.style.background = "#4d2065") {
+        besCavab.style.background = "green"
+        besCavab.innerHTML = "Cavab doğrudur-next(2click) "
+    } else {
+        alert("hey")
+    }
+}
+
+
+// loopa salmaliam bu gediw yaxshi gedirw deil
+// 
+// 
+
+
+
+
+function duzgunn1() {
     if (ikiSual.style.display = "none") {
         ikiSual.style.display = "block"
         ucSual.style.display = "none"
@@ -25,8 +111,78 @@ function duzgun() {
     }
 }
 
-//     x.style.transform = "translate(0,-350px)"
-//     x.innerHTML = "Uğurlar!";
-//     x.style.transition = "1s ease";
-//     setTimeout('window.open(\'http://127.0.0.1:5500/game\')', 5000)
-//
+function duzgunn2() {
+    if (ucSual.style.display = "none") {
+        ucSual.style.display = "block"
+        ikiSual.style.display = "none"
+        dordSual.style.display = "none"
+        besSual.style.display = "none"
+        birSual.style.display = "none"
+
+    } else {
+        alert("sehv cavabdi")
+
+    }
+}
+
+
+function duzgunn3() {
+    if (dordSual.style.display = "none") {
+        dordSual.style.display = "block"
+        ucSual.style.display = "none"
+        ikiSual.style.display = "none"
+        besSual.style.display = "none"
+        birSual.style.display = "none"
+
+    } else {
+        alert("sehv cavabdi")
+
+    }
+}
+
+function duzgunn4() {
+    if (besSual.style.display = "none") {
+        besSual.style.display = "block"
+        ikiSual.style.display = "none"
+        dordSual.style.display = "none"
+        ucSual.style.display = "none"
+        birSual.style.display = "none"
+
+    } else {
+        alert("sehv cavabdi")
+
+    }
+}
+
+function duzgunn5() {
+
+    alert("Tebrikler udduz")
+}
+
+// function duzgunn1() {
+//     if (ikiSual.style.display = "none") {
+//         ikiSual.style.display = "block"
+//         ucSual.style.display = "none"
+//         dordSual.style.display = "none"
+//         besSual.style.display = "none"
+//         birSual.style.display = "none"
+
+//     } else {
+//         alert("sehv cavabdi")
+
+//     }
+// }
+
+// function duzgunn1() {
+//     if (ikiSual.style.display = "none") {
+//         ikiSual.style.display = "block"
+//         ucSual.style.display = "none"
+//         dordSual.style.display = "none"
+//         besSual.style.display = "none"
+//         birSual.style.display = "none"
+
+//     } else {
+//         alert("sehv cavabdi")
+
+//     }
+// }
